0a6410af60495bc7e5162bca9744e0f238d590ce
DmitryT <todem5@gmail.com>
2024-05-05 13:04:57 +0300

    first commit

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php
+
+// для теста 
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=192.168.3.3;dbname=studbase',
+    //'emulatePrepare' => true,
+    'username' => 'gal_omgtu',
+    'password' => '1#sOf3',
+    'charset' => 'utf8',
+    'tablePrefix' => 'gal_',
+    //'enableProfiling' => (defined('YII_DEBUG') && YII_DEBUG),
+    //'enableParamLogging' => (defined('YII_DEBUG') && YII_DEBUG),
+];
