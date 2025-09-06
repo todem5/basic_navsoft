169a5bb30421407dcd1b75a499e1a61166e6a9d7
todem5 <todem5@gmail.com>
2025-09-06 22:59:08 +0600

    2

--- config/db.php
+++ config/db.php
@@ -4,12 +4,12 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=192.168.3.3;dbname=studbase',
-    //'emulatePrepare' => true,
-    'username' => 'gal_omgtu',
-    'password' => '1#sOf3',
+    'dsn' => 'mysql:host=mysql;dbname=yii',
+    'username' => 'yii',
+    'password' => 'yii',
     'charset' => 'utf8',
-    'tablePrefix' => 'gal_',
     //'enableProfiling' => (defined('YII_DEBUG') && YII_DEBUG),
     //'enableParamLogging' => (defined('YII_DEBUG') && YII_DEBUG),
+];
+
 ];
